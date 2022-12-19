0121fbe3bc5d4a3e3afc525f87419924c3fb4a82
DAnomaly <parksh1219@naver.com>
2022-12-19 13:05:34 +0900

    RegistForm 기능 추가

--- SetTimeGenerateFile/SetTimeGenerateFile/RegistForm.cs
+++ SetTimeGenerateFile/SetTimeGenerateFile/RegistForm.cs
@@ -1,12 +1,17 @@
-﻿namespace SetTimeGenerateFile
+﻿using ControlQueue;
+
+namespace SetTimeGenerateFile
 {
     public partial class RegistForm : Form
     {
+        #region 1. 생성자
         public RegistForm()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region 2. 메소드
         /// <summary>
         /// 리스트 보기 버튼
         /// </summary>
@@ -26,7 +31,55 @@
         /// <param name="e"></param>
         private void GenerateButton_Click(object sender, EventArgs e)
         {
-            
+            // 파일명 확인
+            string filename = FileNameBox.Text;
+            if(    filename == null 
+                || filename.Trim().Length == 0)
+            {
+                MessageBox.Show("파일명을 입력해주세요.");
+                FileNameBox.Focus();
+                return;
+            }
+            if( filename.LastIndexOf(".") == -1 )
+            {
+                filename += ".dat";
+            }
+
+            // 내용 확인
+            string content = FileContentBox.Text;
+            if (   content == null
+                || content.Trim().Length == 0)
+            {
+                MessageBox.Show("내용을 입력해주세요.");
+                FileContentBox.Focus();
+                return;
+            }
+
+            // 시간 확인
+            DateTime genDate = SetDateTime.Value;
+            if( DateTime.Now > genDate )
+            {
+                MessageBox.Show("설정하신 시간이 이미 지났습니다.");
+                SetDateTime.Focus();
+                return;
+            }
+
+            // 대기열 생성
+            try
+            {
+                ControlQueue.Control.InsertQueue(filename, content, genDate);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("대기열 생성중 오류가 발생했습니다.\r\n" + ex.Message);
+                return;
+            }
+            MessageBox.Show(filename + " 대기열이 생성되었습니다.");
+
+            // 내용 초기화
+            FileNameBox.Text = "";
+            FileContentBox.Text = "";
         }
+        #endregion
     }
 }
